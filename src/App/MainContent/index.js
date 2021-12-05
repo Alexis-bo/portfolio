ed27f8f10d33d35ad62f4ebd133e0b472fc3fb71
Alexis-bo <alexis.bonnel@etu.utc.fr>
2021-12-05 12:43:31 +0100

    feat/on-click-and-active-items-topbar

--- src/App/MainContent/index.js
+++ src/App/MainContent/index.js
@@ -1,45 +1,16 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
 import React, { Fragment } from 'react';
-import { SectionContainer, SectionTitle } from '../Components/style';
 
-import Fastlearner from './views/Fastlearner';
-import Languages from './views/Languages';
-import Openminded from './views/Openminded';
-import Opportunities from './views/Opportunities';
-import Presentation from './views/Presentation';
-import Project from './views/Projects';
-import Resume from './views/Resume';
-import Versatile from './views/Versatile';
+import AboutmeSection from './views/AboutmeSection';
+import ContactSection from './views/ContactSection';
+import ProjectsSection from './views/ProjectsSection';
 
 const MainContent = () => (
   <Fragment>
-    <SectionContainer>
-      <Presentation />
-    </SectionContainer>
-
-    <SectionContainer horizontal>
-      <Versatile />
-      <Fastlearner />
-      <Openminded />
-    </SectionContainer>
-
-    <SectionContainer>
-      <Languages />
-    </SectionContainer>
-
-    <SectionContainer>
-      <SectionTitle>A few projects I have worked on</SectionTitle>
-      <Project />
-    </SectionContainer>
-
-    <SectionContainer>
-      <Opportunities />
-    </SectionContainer>
-
-    <SectionContainer last>
-      <Resume />
-    </SectionContainer>
+    <AboutmeSection />
+    <ProjectsSection />
+    <ContactSection />
   </Fragment>
 );
 
