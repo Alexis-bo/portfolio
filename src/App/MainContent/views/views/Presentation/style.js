9d6d6377dcbdb24d35cc6535b6dd26584e23a941
Alexis-bo <alexis.bonnel@etu.utc.fr>
2021-12-05 15:53:16 +0100

    feat/disable-staircase-and-center-on-mobile

--- src/App/MainContent/views/views/Presentation/style.js
+++ src/App/MainContent/views/views/Presentation/style.js
@@ -3,12 +3,22 @@ import { styled } from 'styletron-react';
 const PresentationText = styled('div', ({ leftMargin }) => ({
   // display: 'flex',
   marginLeft: leftMargin,
+  '@media only screen and (min-width: 300px) and (max-width: 1025px)': {
+    marginLeft: '0px',
+  },
 }));
 
 const PresentationTextContainer = styled('div', () => ({
   display: 'flex',
   flexDirection: 'column',
   color: '#b7b3b0',
+  '@media only screen and (min-width: 300px) and (max-width: 1025px)': {
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    fontSize: '60px',
+    lineHeight: '80px',
+  },
 }));
 
 export { PresentationText, PresentationTextContainer };
